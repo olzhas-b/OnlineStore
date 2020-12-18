ceb461f68f86736b31b7bf4e0f5980d938bc3e11
qwerty321kz <oljas.bazarbekov15@gmail.com>
2020-12-19 02:09:08 +0600

    changed logo name

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+// hello it is me
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
